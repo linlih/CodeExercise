95c818d322a4a8942396bdd6aa051d272a73464e
linlih <linhome.luck@gmail.com>
2018-12-26 11:34:33 +0800

    update graph

---
+++ Graph/graph.h
@@ -0,0 +1,93 @@
+//*********** Graph.h **************//
+// 图的基类
+
+#define UNVISITED 0
+#define VISITED   1
+#define INFINITE 0xffffffff
+
+class Edge {
+public:
+    int weight; // 边的权值
+    int from;   // 边的起点
+    int to;     // 边的终点
+    
+    Edge(){
+        from = -1; to = -1; weight = 0;
+    }
+    Edge(int f, int t, int w){
+        from = f; to = t; weight = w;
+    }
+    bool operator < (const Edge &arg) {return  (this->weight < arg.weight);};
+    bool operator == (const Edge &arg) {return  (this->weight == arg.weight);};
+    bool operator > (const Edge &arg) {return  (this->weight > arg.weight);};
+    bool operator <= (const Edge &arg) {return  (this->weight <= arg.weight);};
+    bool operator >= (const Edge &arg) {return  (this->weight >= arg.weight);};
+};
+
+class Graph {
+public:
+    int numVertex; // 图顶点的个数
+    int numEdge;   // 图的边的数目
+    int *Mark;     // 标记节点是否访问的数组
+    int *Indegree; // 保存图顶点的入度数组
+
+    Graph(int numVert) {
+        numVertex = numVert;
+        numEdge = 0;
+        Indegree = new int[numVertex];
+        Mark = new int[numVertex];
+
+        for(int i = 0; i < numVertex; i++){
+            Mark[i] = UNVISITED;
+            Indegree[i] = 0;
+        }
+    }
+
+    ~Graph() {
+        delete [] Mark;
+        delete [] Indegree;
+    }
+
+    virtual Edge FirstEdge(int oneVertex) {
+        Edge myEdge;
+        myEdge.from = oneVertex;
+        myEdge.to = -1;
+        return myEdge;
+    }
+
+    virtual Edge NextEdge(Edge preEdge) {
+        return preEdge;
+    }
+
+    int VerticesNum() {
+        return numVertex;
+    }
+
+    int EdgesNum() {
+        return numEdge;
+    }
+
+    int FromVertex(Edge oneEdge) {
+        return oneEdge.from;
+    }
+
+    int ToVertex(Edge oneEdge){
+        return oneEdge.to;
+    }
+
+    int Weight(Edge oneEdge) {
+        return oneEdge.weight;
+    }
+
+    bool IsEdge(Edge oneEdge) {
+        // 是边的条件要满足权值大于零小于INFINITE并且有边的终点
+        if  (oneEdge.weight > 0 && oneEdge.weight < INFINITE && oneEdge.to >= 0)
+            return true;
+        else 
+            return false;
+    }
+
+    virtual void setEdge(int from, int to, int weight) = 0;
+    virtual void delEdge(int from, int to) = 0;
+};
+
---
+++ Graph/graphl.h
@@ -0,0 +1,160 @@
+// 图的邻接表实现
+
+#include "graph.h"
+
+#define UNVISITED 0
+#define VISITED   1
+#define INFINITE 0xffffffff
+#define N  5 // 定义图的顶点数目
+
+using namespace std;
+
+struct listUnit { // 邻接表表目中数据结构部分的结构定义
+    int vertex; // 边的终点
+    int weight; // 边的权
+};
+
+template<class Elem>
+class Link {
+public:
+    Elem element;
+    Link *next;
+    Link(const Elem& elemval, Link *nextval = NULL) {
+        element = elemval;
+        next = nextval;
+    }
+    Link(Link *nextval = NULL) {
+        next = nextval;
+    }
+};
+
+template<class Elem>
+class LList {
+    Link<Elem>* head;
+    LList() {
+        head = new Link<Elem>();
+    }
+
+    void removeall() {
+        Link<Elem> *fence;
+        while(head != NULL){
+            fence = head;
+            head = head->next;
+            delete fence;
+        }
+    }
+    ~LList() {
+        removeall();
+    }
+};
+
+class Graphl: public Graph {
+private:
+    LList<listUnit> *graList; // 保存所有边表的数组
+public:
+    Graphl(int numVert):Graph(numVert) {
+        graList = new LList<listUnit>[numVert];
+    }
+
+    ~Graphl() {
+        delete [] graList;
+    }
+
+    Edge FirstEdge(int oneVertex) {
+        Edge myEdge;
+        myEdge.from = oneVertex;
+        Link<listUnit> *temp = graList[oneVertex].head;
+        if(temp->next != NULL) {
+            myEdge.to = temp->next->element.vertex;
+            myEdge.weight = temp->next->element.weight;
+        }
+
+        // 如果找到了顶点oneVertex的第一条边，则返回的myEdge确实是一条边;如果么有找到顶点的oneVertex的第一条边，那么myEdge的成员变量to为-1，根据IsEgde函数可以判断myEdge不是一条边
+        return myEdge;
+    }
+
+    Edge NextEdge(Edge preEdge) {
+        Edge myEdge;
+        myEdge.from = preEdge.from;
+        Link<listUnit> *temp = graList[preEdge.from].head;
+        while (temp->next != NULL && temp->next->element.vertex <= preEdge.to)
+            temp = temp->next;
+        if (temp->next != NULL) {
+            myEdge.to = temp->next->element.vertex;
+            myEdge.weight = temp->next->element.weight;
+        }
+        return myEdge;
+    }
+
+    void setEdge(int from, int to, int weight) {
+        Link<listUnit> *temp = graList[from].head;
+
+        while (temp->next != NULL && temp->next->element.vertex < to)
+            temp = temp->next;
+        
+        // 表中不存在这条边，则添加一条边
+        if (temp->next == NULL) {
+            temp->next = new Link<listUnit>;
+            temp->next->element.vertex = to;
+            temp->next->element.weight = weight;
+            numEdge ++;
+            Indegree[to] ++;
+            return;
+        }
+
+        // 如果表中已经存在了这条边的话，直接修改对应的权值即可
+        if (temp->next->element.vertex == to){
+            temp->next->element.weight = weight;
+            return;
+        }
+
+        // 边(from, to)或<from, to>在边表中不存在，但在边表中其后存在其他边，则在边表中插入这条边
+        if (temp->next->element.vertex > to)
+        {
+            Link<listUnit> *other = temp->next;
+            temp->next = new Link<listUnit>;
+            temp->next->element.vertex = to;
+            temp->next->element.weight = weight;
+            temp->next->next = other;
+            numEdge ++;
+            Indegree[to] ++;
+            return;
+        }
+    }
+
+    void delEdge(int from, int to) {
+        Link<listUnit> *temp = graList[from].head;
+        
+        while(temp->next != NULL && temp->next->element.vertex < to)
+            temp = temp->next;
+        
+        if ((temp->next == NULL) || (temp->next->element.vertex > to))
+            return;
+
+        if (temp->next->element.vertex == to) {
+            Link<listUnit> *other = temp->next->next;
+            delete temp->next;
+            temp->next = other;
+            numEdge --;
+            Indegree[to] --;
+            return;
+        }  
+    }
+
+    void InitGraphl(Graphl *graphl, int A[N][N]);
+    void DFS(Graph& G, int v);
+    void BFS(Graph& G, int v);
+    void Vist(Graph& G, int v);
+};
+
+void Graphl::InitGraphl(Graphl *graphl, int A[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+
+        }
+    }
+}
+
+
+
+
