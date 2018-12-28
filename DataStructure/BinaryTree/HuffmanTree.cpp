4d5a14d773ef20ff65134147a8908efd2bf0a938
linlih <linhome.luck@gmail.com>
2018-12-28 20:17:30 +0800

    update huffman

--- DataStructure/BinaryTree/HuffmanTree.cpp
+++ DataStructure/BinaryTree/HuffmanTree.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    // 图示来自张铭老师《数据结构与算法》P128, 图 5.19
     int weight[] = {6, 2, 3, 4};
 
     HuffmanTree<int> a(weight, 4);
@@ -17,3 +18,4 @@ int main(int argc, char const *argv[])
 }
 
 
+    
