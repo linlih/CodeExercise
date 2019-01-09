51fd1d369c639aec727014595b79f26d7bd438ef
linlih <linhome.luck@gmail.com>
2019-01-09 22:03:51 +0800

    update

--- Bailian_oj/1657.cpp
+++ Bailian_oj/1657.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 int main(int argc, char const *argv[])
@@ -7,19 +8,42 @@ int main(int argc, char const *argv[])
     int n;
     string a, b;
     int x1, y1, x2, y2;
+    int king = 0, queen = 0, car = 0, elep = 0; // 定义最短距离
+
     cin >> n;
     while (n > 0) {
         cin >> a >> b;
-        x1 = a.at(0) - 'a' + 1;
-        y1 = a.at(1) - '1' + 1;
-        x2 = b.at(0) - 'a' + 1;
-        y2 = b.at(1) - '1' + 1;
-        cout << x1 << y1 << " " << x2 << y2 << endl;
+        if  (a == b)
+            cout << "0 0 0 0" << endl;
+        else {
+            x1 = a.at(0) - 'a' + 1;
+            y1 = a.at(1) - '1' + 1;
+            x2 = b.at(0) - 'a' + 1;
+            y2 = b.at(1) - '1' + 1;
+            //cout << x1 << y1 << " " << x2 << y2 << endl;
+            if (abs(x2-x1) > abs(y2-y1))
+                king = abs(x2-x1);
+            else 
+                king = abs(y2-y1);
+            if ((abs(x2-x1) == abs(y2-y1)) || ((x1 == x2) || (y1 == y2)))        
+                queen = 1;
+            else 
+                queen = 2;
+            if ((x1 == x2) || (y1 == y2))
+                car = 1;
+            else
+                car = 2;
+            cout << king << " " << queen << " " << car << " " ;// << (abs(x1-x2) == abs(y1-y2))?"1":"Inf" << endl; 
+            if (abs(x1-x2) == abs(y1-y2))
+                cout << "1" << endl;
+            else if ((abs(x1-x2)+abs(y1-y2))%2 == 0)
+                cout << "2" << endl;
+            else
+                cout << "Inf" << endl;
+        }
         n--;
     }
-    // string str("a1");
-    // cout << str.at(0) << " " << str.at(1) <<endl;
-    // int t = str.at(0) - 'a';
-    // cout << t << endl;
+    
+
     return 0;
 }
