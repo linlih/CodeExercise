465ef4d856d88694853b003779f6dc997e125699
linlih <linhome.luck@gmail.com>
2019-01-17 21:44:03 +0800

    update

---
+++ Bailian_oj/2677.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    int i, j;
+    cin >> n;
+    int g;
+    int area = 0;
+    int tmp_i, tmp_j;
+    int **arr; // 数组指针
+    arr = (int**)malloc(sizeof(int*)*n);  
+    for(i = 0;i < r;i++)  
+        arr[i] = (int*)malloc(sizeof(int)*n);
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            cin >> g;
+            a[i][j] = 
+            if (g <= 50) {
+                area ++;
+                tmp_i = i;
+                tmp_j = j;
+            }
+
+            
+        }
+    } 
+    return 0;
+}
--- Bailian_oj/2713.cpp
+++ Bailian_oj/2713.cpp
@@ -1,8 +1,39 @@
-#include <iostream>
 
+#include <cstdio>
+#include <iostream>
 using namespace std;
-int main(int argc, char const *argv[])
+
+/*
+* 说明，这个代码只适用于图像中只包含了一个矩阵块的情况
+* 2713这个题目没有明确说明只有一个矩形区域，这个代码可以通过的话，说明就是只有一个
+* 如果图像中包含有几个矩阵区域的情况，就会比较负责了，需要再考虑
+*/
+
+int main()
 {
-    
+    int i, j, n, p, sx, sy, tx, ty, x, y;
+    cin >> n;
+    sx = sy = n; // 标记矩阵右下角
+    tx = ty = 0; // 标记矩阵左上角
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            cin >> p;
+            if (p == 0) {
+                if (i + j < sx + sy) {
+                    sx = i;
+                    sy = j;
+                }
+                if (i + j > tx + ty) {
+                    tx = i;
+                    ty = j;
+                }
+            }
+        }
+    }
+    x = tx - sx + 1;
+    y = ty - sy + 1;
+    x -= 2;
+    y -= 2;
+    cout << x * y << endl;
     return 0;
 }
