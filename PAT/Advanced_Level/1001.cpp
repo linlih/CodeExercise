db4d2ba7ced6e7c491af02edd2d6b6f2715a1f0e
linlih <linhome.luck@gmail.com>
2019-06-28 23:38:28 +0800

    update

---
+++ PAT/Advanced_Level/1001.cpp
@@ -0,0 +1,31 @@
+/*
+ * 运行数据： ms  KB
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    long a, b;
+    cin >> a >> b;
+    long c = a + b;
+    char res[100] = {0};
+    //itoa(c, res, 10);
+    snprintf(res, sizeof(res), "%d", c);
+    int cnt = 0;
+    stack<char> s;
+    for (int i = strlen(res) - 1; i >= 0; --i) {
+        if (cnt%3==0 && cnt > 0  && res[i] != '-') {
+            s.push(',');
+            //cout << "cnt" << cnt << endl;
+        }
+        cnt ++;
+        s.push(res[i]);
+    }
+    while(!s.empty()) {
+        cout << s.top();
+        s.pop();
+    }
+    cout << endl;
+    return 0;
+}
