9b06d3ef18b34997a9895406aab11829e7d2ed0d
linlih <linhome.luck@gmail.com>
2019-07-06 23:03:17 +0800

    update

---
+++ PAT/Advanced_Level/1007.cpp
@@ -0,0 +1,30 @@
+/*
+   问题描述：
+   解题思路： 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    scanf("%d", &n);
+    vector<int> v(n);
+    int leftindex = 0, rightindex = n-1, sum = -1, temp = 0, tempindex = 0;
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &v[i]);
+        temp = temp + v[i];
+        if (temp < 0) {
+            temp = 0; 
+            tempindex = i + 1;
+        }
+        else if (temp > sum) {
+            sum = temp;
+            leftindex = tempindex;
+            rightindex = i;
+        }
+    } 
+    if (sum < 0) sum = 0;
+    cout << sum << " " << v[leftindex] << " " << v[rightindex] << endl;
+    return 0;
+}
