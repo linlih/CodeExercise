bf9d012bc0f7e2577f0e4aa0988c27cc931ddf6f
linlih <linhome.luck@gmail.com>
2019-07-09 22:59:56 +0800

    update

--- PAT/Advanced_Level/1009.cpp
+++ PAT/Advanced_Level/1009.cpp
@@ -2,35 +2,35 @@
    问题描述：
    解题思路： 
  */
+
+#if 1 // 错误解法
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef struct num{
     int exp;
-    float coef;
+    double coef;
 }num;
 
 int main(int argc, char const *argv[])
 {
     int cnt_a, cnt_b;
-    float cal[1001] = {0};
-    int tmp_i;
-    float tmp_n;
+    //double cal[2001] = {0};
     
     scanf("%d", &cnt_a);
     num *a = new num[cnt_a];
     for (int i = 0; i < cnt_a; ++i) {
-        scanf("%d %f", &a[i].exp, &a[i].coef);
+        scanf("%d %lf", &a[i].exp, &a[i].coef);
     }
 
     scanf("%d", &cnt_b);
     num *b = new num[cnt_b];
     for (int i = 0; i < cnt_b; ++i) {
-        scanf("%d %f", &b[i].exp, &b[i].coef);
+        scanf("%d %lf", &b[i].exp, &b[i].coef);
     }
 
-    num *res = new num[cnt_a + cnt_b];
+    num *res = new num[cnt_a + cnt_b + 1];
     int res_i = 0;
     for (int i = 0; i < cnt_a; ++i) {
         for (int j = 0; j < cnt_b; ++j) {
@@ -40,9 +40,40 @@ int main(int argc, char const *argv[])
     }
     int cnt = res_i - 1;
     cout << cnt;
-    for (int i = cnt; i > 0; --i) {
-        if (res[i].coef > 0)
+    for (int i = cnt_a + cnt_b; i > 0; --i) {
+        if (res[i].coef != 0)
             printf(" %d %.1f", i, res[i].coef);
     }
     return 0;
 }
+#else
+#include <iostream>
+using namespace std;
+int main() {
+    int n1, n2, a, cnt=0;
+    double b, arr[1001] = {0.0}, ans[2001] = {0.0};
+
+    scanf("%d", &n1);
+    for (int i = 0; i < n1; ++i) {
+        scanf("%d %lf", &a, &b);
+        arr[a] = b;
+    }
+    
+    scanf("%d", &n2);
+    for (int i = 0; i < n2; ++i) {
+        scanf("%d %lf", &a, &b);
+        for (int j = 0; j < 1001; ++j) {
+            ans[j + a] += arr[j] * b;
+        } 
+    }
+    for (int i = 2000; i >= 0; i--) {
+        if (ans[i] != 0.0) cnt ++;
+    }
+    printf("%d", cnt);
+    for (int i = 2000; i >= 0; i--)
+        if (ans[i] != 0.0) {
+            printf(" %d %.1f", i, ans[i]);
+        }
+    return 0;
+}
+#endif
