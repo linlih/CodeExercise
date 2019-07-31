69d3e6816a039429c0f5bb33a52dbacb87077e21
linlih <linhome.luck@gmail.com>
2019-07-31 23:19:50 +0800

    path

---
+++ PAT/Advanced_Level/1030.cpp
@@ -0,0 +1,31 @@
+/*
+   问题描述：
+   解题思路： 
+ */
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int n, m, s, d;
+int e[510][510], dis[510], cost[510][510];
+vector<int> pre[510];
+cost int inf = 99999999;
+vector<int> path, temppath;
+int mincost = inf;
+
+void dfs(int v) {
+    temppath.push_back(v);
+    if (v == s) {
+        int tempcost = 0;
+        for (int i = temppath.size() - 1; i >0; i-- ) {
+            ind id = temppath[]
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
