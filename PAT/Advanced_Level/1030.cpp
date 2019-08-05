0203804334aa1211602896c1101c9d9324081acd
linlih <linhome.luck@gmail.com>
2019-08-05 22:50:57 +0800

    update

--- PAT/Advanced_Level/1030.cpp
+++ PAT/Advanced_Level/1030.cpp
@@ -10,7 +10,8 @@ using namespace std;
 int n, m, s, d;
 int e[510][510], dis[510], cost[510][510];
 vector<int> pre[510];
-cost int inf = 99999999;
+bool visit[510];
+const int inf = 99999999;
 vector<int> path, temppath;
 int mincost = inf;
 
@@ -18,14 +19,65 @@ void dfs(int v) {
     temppath.push_back(v);
     if (v == s) {
         int tempcost = 0;
-        for (int i = temppath.size() - 1; i >0; i-- ) {
-            ind id = temppath[]
+        for (int i = temppath.size() - 1; i > 0; i-- ) {
+            int id = temppath[i];
+            int nextid = temppath[i-1];
+            tempcost += cost[id][nextid];
         }
+        if (tempcost < mincost) {
+            mincost = tempcost;
+            path = temppath;
+        }
+        temppath.pop_back();
+        return;
     }
+    for (int i = 0; i < pre[v].size(); i++) {
+        dfs(pre[v][i]);
+    }
+    temppath.pop_back();
 }
 
 int main(int argc, char const *argv[])
 {
-    
+    fill(e[0], e[0] + 510*510, inf);
+    fill(dis, dis + 510, inf);
+    scanf("%d %d %d %d", &n, &m, &s, &d);
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        scanf("%d", &e[a][b]);
+        e[b][a] = e[a][b];
+        scanf("%d", &cost[a][b]);
+        cost[b][a] = cost[a][b];
+    }
+    pre[s].push_back(s);
+    dis[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int u = -1, minn = inf;
+        for (int j = 0; j < n; j++) {
+            if (visit[j] == false && dis[j] < minn) {
+                u = j;
+                minn = dis[j];
+            }
+        }
+        if (u == -1) break;
+        visit[u] = true;
+        for (int v = 0; v < n; v++) {
+            if (visit[v] == false && e[u][v] != inf) {
+                if (dis[v] > dis[u] + e[u][v]) {
+                    dis[v] = dis[u] + e[u][v];
+                    pre[v].clear();
+                    pre[v].push_back(u);
+                }else if (dis[v] == dis[u] + e[u][v]) {
+                    pre[v].push_back(u);
+                }
+            }
+        }
+    }
+    dfs(d);
+    for (int i = path.size() - 1; i >= 0; i--) {
+        printf("%d ", path[i]);
+    }
+    printf("%d %d", dis[d], mincost);
     return 0;
 }
