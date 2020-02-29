ac17724f9d2f3b89042df7be3ce6acf467c37b2e
linlih <linhome.luck@gmail.com>
2020-02-29 22:00:58 +0800

    update

---
+++ PAT/Advanced_Level/1027.cpp
@@ -0,0 +1,36 @@
+/*
+   问题描述：
+   解题思路： 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    string out;
+    out.push_back('#');
+
+    int a;
+    string tmp;
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &a);
+        while(a != 0) {
+            if (a % 13 < 10)
+                tmp.push_back(a % 13 + '0');
+            else 
+                tmp.push_back(a % 13 + 'A' - 10);
+            a = a / 13;
+        }
+        if (tmp.size() == 1)
+            out += '0';
+        if (tmp.size() == 0)
+            out += "00";
+        out.append(tmp.rbegin(), tmp.rend());
+        
+        tmp.clear();
+    }
+
+    cout << out << endl;
+    
+    return 0;
+}
