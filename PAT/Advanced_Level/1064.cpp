19cbe533870a4d5b2b36111d76c27783c0c464bd
linlih <linhome.luck@gmail.com>
2020-06-04 21:38:06 +0800

    update

---
+++ PAT/Advanced_Level/1064.cpp
@@ -0,0 +1,28 @@
+/*
+   问题描述：
+   解题思路： 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int in[1010], level[1010], n, t = 0;
+void inOrder(int root) {
+    if (root >= n) return;
+    inOrder(root * 2 + 1);
+    level[root] = in[t++];
+    inOrder(root * 2 + 2);
+}
+int main(int argc, char const *argv[]) {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &in[i]);
+    }
+    sort(in, in + n);
+    inOrder(0);
+    printf("%d", level[0]);
+    for (int i = 1; i < n; ++i) {
+        printf(" %d", level[i]);
+    } 
+    return 0;
+}
