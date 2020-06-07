4cfe11dfbf40b562fede5085aaa4425458b2e2a4
linlih <linhome.luck@gmail.com>
2020-06-07 19:38:36 +0800

    update

--- PAT/Advanced_Level/1073.cpp
+++ PAT/Advanced_Level/1073.cpp
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[]) {
     int i = 0;
     while (s[i] != 'E') ++i;
     string t = s.substr(1, i-1);
-    int n = stoi(s.substr(i+1));
+    int n = stoi(s.substr(i+1)); // 取指数部分
     if (s[0] == '-') cout << '-';
     if (n < 0) {
         cout << "0.";
@@ -19,10 +19,12 @@ int main(int argc, char const *argv[]) {
         for (int j = 0; j < t.length(); j++)
             if (t[j] != '.') cout << t[j];
     }
-    else {
-        cout << t[0];
+    else { // 这个实现只能转换x.xxxxExx的形式，如果是xx.xxxxExxx则出错
+        cout << t[0]; //
         int cnt, j;
+        // j从2开始跳过小数点
         for (j = 2, cnt = 0; j < t.length() && cnt < n; j++, cnt++) cout << t[j];
+        // +1.23000E01 输出结果为：12.3000
         if (j == t.length()) {
             for (int k = 0; k < n - cnt; k++) cout <<'0';
         }
