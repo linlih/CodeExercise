02baf494b242674f6a52b607ae59c150343dbd41
linlih <linhome.luck@gmail.com>
2020-06-29 18:02:19 +0800

    update

--- PAT/Advanced_Level/1090.cpp
+++ PAT/Advanced_Level/1090.cpp
@@ -1,12 +1,39 @@
 /*
-   问题描述：
+   问题描述：没看懂题目。。。
    解题思路： 
  */
 #include <bits/stdc++.h>
 
 using namespace std;
+
+int n, maxdepth = 0, maxnum = 0, temp, root;
+vector<int> v[10010];
+
+void dfs(int index, int depth) {
+    if (v[index].size() == 0) {
+        if (maxdepth == depth)
+            maxnum++;
+        if (maxdepth < depth) {
+            maxdepth = depth;
+            maxnum = 1;
+        }
+        return;
+    }
+    for (int i = 0; i < v[index].size(); ++i)
+        dfs(v[index][i], depth + 1);
+}
+
 int main(int argc, char const *argv[]) {
     freopen("input.txt","r",stdin);
-    
+    double p, r;
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &temp);
+        if (temp == -1)
+            root = i;
+        else 
+            v[temp].push_back(i);
+    }
+    dfs(root, 0);
+    printf("%.2f %d", p * pow(1 + r/100, maxdepth), maxnum);
     return 0;
 }
