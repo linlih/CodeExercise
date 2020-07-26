0ab41acfccb80b286f140a53a2dcc3a6f95e13c9
linlih <linhome.luck@gmail.com>
2020-07-26 23:33:24 +0800

    update

--- leetcode/98. Validate Binary Search Tree.cpp
+++ leetcode/98. Validate Binary Search Tree.cpp
@@ -16,10 +16,33 @@ class Solution {
 public:
     bool isValidBST(TreeNode* root, TreeNode* min=NULL, TreeNode* max=NULL) {
         if (root == NULL) return true;
-        //if (root->left != NULL && root->val <= root->left->val) return false;
-        //if (root->right != NULL && root->val >= root->right->val) return false;
         if (min != NULL && root->val <= min->val) return false;
         if (max != NULL && root->val >= max->val) return false;
         return isValidBST(root->left, min, root) && isValidBST(root->right, root, max);
+    }
+};
+
+// 自己的思路，利用BST数的性质：中序遍历得到的是一个有序序列
+class Solution {
+public:
+    //vector<int> seq;
+    void inorder(TreeNode* root, vector<int> &seq) {
+        if (root == NULL) return;
+        inorder(root->left, seq);
+        seq.push_back(root->val);
+        inorder(root->right, seq);
+    }
+    bool isValidBST(TreeNode* root) {
+        // 利用BST树中序是有序的来实现
+        vector<int> seq;
+        inorder(root, seq);
+        bool res = true;
+        if (seq.size() > 1) // 这里要注意判断，处理输入为空或者1个节点的情况
+            for (int i = 0; i < seq.size() - 1; ++i) {
+                if (seq[i] >= seq[i+1]) {
+                    res = false; break;
+                }
+            }
+        return res;
     }
 };
